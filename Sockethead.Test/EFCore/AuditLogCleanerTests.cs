e26da6d7676d5840d729afd228886c1038531149
Munesh Kumar <muneshkumar578@gmail.com>
2023-03-08 14:43:49 +0500

    Sockethead.Test: Added Tests for Audit Log Cleaner

---
+++ Sockethead.Test/EFCore/AuditLogCleanerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Sockethead.EFCore.AuditLogging;
+using Sockethead.Test.Common;
+using Xunit;
+
+namespace Sockethead.Test.EFCore
+{
+    public class AuditLogCleanerTests : IClassFixture<TestsFixture>
+    {
+        private TestsFixture Fixture { get; }
+
+        public AuditLogCleanerTests(TestsFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+        [Fact]
+        public async Task OldAuditLogsShouldBeDeleted()
+        {
+            AuditLogger auditLogger = Fixture.GetService<AuditLogger>();
+            AuditLogCleaner auditLogCleaner = Fixture.GetService<AuditLogCleaner>();
+        
+            DateTime oldestAllowedAuditLogTime = DateTime.UtcNow.Subtract(auditLogCleaner.GetAuditLogAge());
+            IQueryable<Sockethead.EFCore.Entities.AuditLog> auditLogsQuery = auditLogger.OnlyAuditLog
+                .Where(log => log.TimeStamp < oldestAllowedAuditLogTime);
+        
+            if(!await auditLogsQuery.AnyAsync())
+                return;
+
+            int totalLogsDeleted = await auditLogCleaner.CleanupAuditLogsAsync(CancellationToken.None);
+            totalLogsDeleted.Should().BeGreaterThan(0);
+        
+            Assert.False(await auditLogsQuery.AnyAsync());
+        }
+    }
+}
