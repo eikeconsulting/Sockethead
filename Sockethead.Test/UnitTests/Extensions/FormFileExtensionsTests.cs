d06bd8dde96d4388755c0430f353ecac5780db86
Munesh Kumar <muneshkumar578@gmail.com>
2023-04-03 15:56:35 +0500

    Sockethead.Test: Added unit tests for FormFile extension methods

---
+++ Sockethead.Test/UnitTests/Extensions/FormFileExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http.Internal;
+using Sockethead.Common.Extensions;
+using Xunit;
+
+namespace Sockethead.Test.UnitTests.Extensions
+{
+    public class FormFileExtensionsTests
+    {
+        [Fact]
+        public void ToBase64_ReturnsBase64String()
+        {
+            string fileName = "test.jpg";
+            byte[] fileContent = new byte[] { 0x12, 0x34, 0x56, 0x78 };
+            FormFile file = new FormFile(new MemoryStream(fileContent), 0, fileContent.Length, "test", fileName);
+
+            string result = file.ToBase64();
+
+            Assert.Equal("EjRWeA==", result);
+        }
+    }
+}
